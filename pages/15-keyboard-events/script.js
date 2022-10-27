e57a389838606ff344a5769faa61e806d74e74d6
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 17:24:51 -0500

    Add activity 16

--- pages/15-keyboard-events/script.js
+++ pages/15-keyboard-events/script.js
@@ -2,4 +2,5 @@
 
 // TODO: Update all of the tags in section whenever we type in the textarea ONLY IF it's an alphabetical character (case insensitive)
 // Hint: Use 'keydown' event and 'includes' and 'toLowerCase' methods
+
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
