8daac1acd3bb188c60451af0e80bb0986502489b
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 17:16:35 -0500

    Add 15

---
+++ pages/15-keyboard-events/script.js
@@ -0,0 +1,5 @@
+// TODO: Add class 'font-serif' or 'font-mono' to the section element whenever the select is changed
+
+// TODO: Update all of the tags in section whenever we type in the textarea ONLY IF it's an alphabetical character (case insensitive)
+// Hint: Use 'keydown' event and 'includes' and 'toLowerCase' methods
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
