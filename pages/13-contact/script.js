76e4220ddc22f108a899f1c52f51676b20ae8aa7
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 16:24:13 -0500

    Add 13-form

---
+++ pages/13-contact/script.js
@@ -0,0 +1,3 @@
+// TODO: Handle form submission (prevent default)
+// TODO: Capture the form data into an object and then output message in '#response' element
+// Example: "Thank you for your submission John Doe. We will be in touch shortly using your email: john@doe.com"
