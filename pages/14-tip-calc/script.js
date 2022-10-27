a8415f1006ef7eed6dd45c04058721f6e35fb08a
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 16:33:37 -0500

    Add 14

---
+++ pages/14-tip-calc/script.js
@@ -0,0 +1,3 @@
+// TODO: Get the form submission as an object
+// TODO: Write a function to calculate the total amount
+// TODO: Output the information showing voff, the tip amount and the total in 'p' tags
